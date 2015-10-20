aa0accef62f7a7e03191f1b6eb797c2df26126d3
Michael Rinaldi <mikerinaldi1@yahoo.com>
2015-10-19 21:31:37 -0400

    Initial Commit - basic gliding
    -basic gliding mechanic, might need a little tweaking

---
+++ FalconGlideTemp/Assets/Scripts/scrCamFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scrCamFollow : MonoBehaviour {
+
+    public GameObject falcon;
+    readonly Vector3 zOff = new Vector3(0, 0, -10);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = falcon.transform.position + zOff;
+	}
+}
