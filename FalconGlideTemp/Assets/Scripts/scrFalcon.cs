0e2c1f8376c3bb1bbbf7bca62a4a56da56b117cb
Michael Rinaldi <mikerinaldi1@yahoo.com>
2015-10-21 19:30:23 -0400

    Releasing down moves you up
    -removed ascensionPoints
    -releasing down adds a multiple of your current downward velocity to
    your velocity

--- FalconGlideTemp/Assets/Scripts/scrFalcon.cs
+++ FalconGlideTemp/Assets/Scripts/scrFalcon.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class scrFalcon : MonoBehaviour {
 
     const float DEFAULT_SPEED_X = 3;
+    public float downButtonMultiplier = 5;
+    public float releaseDownMultiplier = 60;
+    public float glidingMultiplier = 7;
     Rigidbody2D rb;
     Vector2 artificialGravity = new Vector2(0, -8);
     Vector2 gravityModifier = new Vector2(0, 0);
@@ -12,25 +15,25 @@ public class scrFalcon : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
+        transform.Translate(5, 0, 0);
 	}
 
     // Update is called once per frame
     void Update() {
         //If you hold down, your falcon dives. Increase gravity and horizontal velocity.
-        //Also, increase ascensionPoints by the amount you move down
         if (Input.GetKey("down")) {
-            gravityModifier = new Vector2(5, -5);
-            ascensionPoints += .1f + ascensionPoints;
-            if (ascensionPoints > 300)
-                ascensionPoints = 300;
+            gravityModifier = new Vector2(1, -1) * downButtonMultiplier;
+        }
+        //if you release down, add a multiple of your current downward velocity to your velocity
+        else if (Input.GetKeyUp("down"))
+        {
+            gravityModifier = new Vector2(0, Mathf.Abs(rb.velocity.y) * releaseDownMultiplier);
         }
         //if you are not holding down, your falcon's wings are spread.
-        //Decrease gravity, add your ascensionPoints to the y velocity (to have your falcon rise),
-        //and clear ascensionPoints
-        else {
-            gravityModifier = new Vector2(0, 7 + ascensionPoints*3);
-            Debug.Log(ascensionPoints);
-            ascensionPoints = 0;
+        //Decrease gravity, 
+        else
+        {
+            gravityModifier = new Vector2(0, glidingMultiplier);
         }
 
         //Total Force
