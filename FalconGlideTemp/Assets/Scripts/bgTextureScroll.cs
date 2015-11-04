bb7c0c9498470c247799d4e1ad19fc068a59f109
Ryan Kosha <kosha.r@husky.neu.edu>
2015-11-04 16:52:17 -0500

    Parallax background scrolling added to Master.scene

---
+++ FalconGlideTemp/Assets/Scripts/bgTextureScroll.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bgTextureScroll : MonoBehaviour {
+
+    public float scrollSpeedX;
+    public Renderer rend;
+    private float offsetX;
+    private float initWorldSpaceX;
+
+    // Use this for initialization
+    void Start () {
+        offsetX = scrollSpeedX * Time.time;
+        rend = GetComponent<Renderer>();
+        initWorldSpaceX = transform.position.x;
+	}
+
+	// Update is called once per frame
+	void Update () {
+        offsetX = scrollSpeedX * (transform.position.x - initWorldSpaceX);
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offsetX, 0));
+	}
+}
