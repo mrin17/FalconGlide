1520a5b0a23f55350d4cc6df0e84ed1de5c453f1
Ryan Kosha <kosha.r@husky.neu.edu>
2015-11-06 13:59:43 -0500

    Added parallax prefab, updated parallax script to contain all layers

--- FalconGlideTemp/Assets/Scripts/bgTextureScroll.cs
+++ FalconGlideTemp/Assets/Scripts/bgTextureScroll.cs
@@ -1,23 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class bgTextureScroll : MonoBehaviour {
 
-    public float scrollSpeedX;
-    public Renderer rend;
+	public List<GameObject> layers = new List<GameObject>();
+	public List<float> speeds = new List<float>();
+    private float scrollSpeedX;
+    private Renderer rend;
     private float offsetX;
     private float initWorldSpaceX;
 
     // Use this for initialization
     void Start () {
         offsetX = scrollSpeedX * Time.time;
-        rend = GetComponent<Renderer>();
         initWorldSpaceX = transform.position.x;
 	}
 
 	// Update is called once per frame
 	void Update () {
-        offsetX = scrollSpeedX * (transform.position.x - initWorldSpaceX);
-        rend.material.SetTextureOffset("_MainTex", new Vector2(offsetX, 0));
+		for(int i = 0; i < layers.ToArray().Length; i++)
+		{
+			rend = layers[i].GetComponent<Renderer>();
+			offsetX = speeds[i] * (transform.position.x - initWorldSpaceX) * .001f;
+			rend.material.SetTextureOffset ("_MainTex", new Vector2 (offsetX, 0));
+		}
 	}
 }
